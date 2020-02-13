2143e98f5cc10008e8bbd1734ec6053c7ec53f9b
francescocretti <francesco.cretti@gmail.com>
2020-02-13 18:02:52 +0100

    first version

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// basic website tempalate
+const updateCCYear = () => {
+  document.querySelectorAll('.cc-year').forEach(span => {
+    const currentYear = new Date().getUTCFullYear();
+    span.innerHTML = currentYear;
+  });
+};
+
+// header
+$('#headerBar').load('/templates/header.html', () => {
+  const loc = window.location.pathname;
+  $('.language a').removeClass('active');
+  if (loc === '/en') {
+    $('#en-lang').addClass('active');
+  } else {
+    $('#ita-lang').addClass('active');
+  }
+
+  updateCCYear();
+});
+
+// footer
+$('#footerBar').load('/templates/footer.html', () => updateCCYear());
